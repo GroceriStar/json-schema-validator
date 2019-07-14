fb5e58ed783a3293e2ba6032c80f5cec73804c26
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2019-07-15 01:02:15 +0300

    prettier

---
+++ dist/index.iife.js
@@ -0,0 +1,35 @@
+var StaticDataWrapper = (function (exports) {
+  'use strict';
+
+  /* global describe, it, expect */
+  var jsonFileNotEmptyTest = function jsonFileNotEmptyTest(file) {
+    describe("tests for ".concat(file), function () {
+      it("".concat(file, " data files returns array"), function () {
+        expect(file).not.toBe('');
+      });
+    });
+  }; //@TODO rename it later
+
+
+  var jsonSchemaTest = function jsonSchemaTest(file, example, schema) {
+    describe("test ".concat(file, " json schema"), function () {
+      it("validates ".concat(file, " json-schema"), function () {
+        expect(example).toMatchSchema(schema);
+      });
+    });
+  };
+
+  var src = {
+    jsonFileNotEmptyTest: jsonFileNotEmptyTest,
+    jsonSchemaTest: jsonSchemaTest
+  };
+  var src_1 = src.jsonFileNotEmptyTest;
+  var src_2 = src.jsonSchemaTest;
+
+  exports.default = src;
+  exports.jsonFileNotEmptyTest = src_1;
+  exports.jsonSchemaTest = src_2;
+
+  return exports;
+
+}({}));
