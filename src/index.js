0981360ad1b796f4495b23c65e779d1199f776de
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2019-07-15 00:09:50 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
-// main entry point
+/* global describe, it, expect */
+const jsonFileNotEmptyTest = (file) => {
+  describe(`tests for ${file}`, () => {
+    it(`${file} data files returns array`, () => {
+      expect(file).not.toBe('')
+    })
+  })
+}
+
+//@TODO rename it later
+const jsonSchemaTest = (file, example, schema) => {
+  describe(`test ${file} json schema`, () => {
+    it(`validates ${file} json-schema`, () => {
+      expect(example).toMatchSchema(schema)
+    })
+  })
+}
+
+module.exports = {
+  jsonFileNotEmptyTest,
+  jsonSchemaTest
+}
