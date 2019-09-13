dadc36e23fa88ae85ac05d044cb9a96b3af88820
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2019-09-14 00:30:33 +0300

    release

---
+++ dist/index.es.js
@@ -0,0 +1,31 @@
+import jestJsonSchema from 'jest-json-schema';
+
+var matchers = jestJsonSchema.matchers;
+expect.extend(matchers);
+
+var jsonFileNotEmptyTest = function jsonFileNotEmptyTest(file) {
+  describe("tests for ".concat(file), function () {
+    it("".concat(file, " data files returns array"), function () {
+      expect(file).not.toBe('');
+    });
+  });
+};
+
+var jsonSchemaTest = function jsonSchemaTest(file, example, schema) {
+  describe("test ".concat(file, " json schema"), function () {
+    it("validates ".concat(file, " json-schema"), function () {
+      expect(example).toMatchSchema(schema);
+    });
+  });
+};
+
+var src = {
+  jsonFileNotEmptyTest: jsonFileNotEmptyTest,
+  jsonSchemaTest: jsonSchemaTest
+};
+var src_1 = src.jsonFileNotEmptyTest;
+var src_2 = src.jsonSchemaTest;
+
+export default src;
+export { src_1 as jsonFileNotEmptyTest, src_2 as jsonSchemaTest };
+//# sourceMappingURL=index.es.js.map
